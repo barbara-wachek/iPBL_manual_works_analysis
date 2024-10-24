c6d37e2793b3af7a64577cd694d4a90350424f85
Barbara Wachek <barbara.wachek@gmail.com>
2024-10-24 15:42:02 +0200

    2024-10-24

--- sources_and_manual_works_stats.py
+++ sources_and_manual_works_stats.py
@@ -28,95 +28,109 @@ def gsheet_to_df(gsheetId, worksheet):
     return df
 
 
+def update_rekordy_pozyskane(df):
+    for index, row in tqdm(df.iterrows()):  
+        try:
+            link = row['LINK']
+            # if not pd.isna(link):  # Check if 'LINK' is not null
+                # link = 'https://docs.google.com/spreadsheets/d/1zOxDVFLvk2ovJEyyKp-Zpw6Vqdqr3WG-nueOLy5cVs4/edit#gid=652340147'
+                # link = None
+                # link = 'https://docs.google.com/spreadsheets/d/1ynR57xAatPRKayLn1ZhQ6s3uP_lXSTria0GtlkuH6TY/edit?gid=652340147#gid=652340147'
+            gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
+            table_df = gsheet_to_df(gsheetId, 'Posts')
+            all_extracted_records = table_df['Link'].dropna().shape[0]
+            df.at[index, 'REKORDY POZYSKANE'] = all_extracted_records
+          
+            # else:
+            #     df.at[index, 'REKORDY POZYSKANE'] = 'None'
+       
+        except (KeyError, TypeError):  
+            df.at[index, 'REKORDY POZYSKANE'] = None
+        except:
+            df.at[index, 'REKORDY POZYSKANE'] = "JAKIŚ BŁĄD" 
+
+    return df 
+
+
 def update_rekordy_zaakceptowane(df):
     for index, row in tqdm(df.iterrows()):  
         try:
             link = row['LINK']
-            if not pd.isna(link):  # Check if 'LINK' is not null
-                gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
-                table_df = gsheet_to_df(gsheetId, 'Posts')
-                all_extracted_records = table_df['Link'].dropna().shape[0]
-                filtered_df = table_df[(table_df['do PBL'] == 'True') & (table_df["Data publikacji"].notna())]
-                accepted_records = len(filtered_df['Link'].tolist())
-                
-                df.at[index, 'REKORDY ZAAKCEPTOWANE'] = accepted_records
-                df.at[index, 'REKORDY POZYSKANE'] = all_extracted_records
-                
-            else:
-                df.at[index, 'REKORDY ZAAKCEPTOWANE'] = None  
-                df.at[index, 'REKORDY POZYSKANE'] = None
+            # if not pd.isna(link):  # Check if 'LINK' is not null
+            # link = 'https://docs.google.com/spreadsheets/d/1zOxDVFLvk2ovJEyyKp-Zpw6Vqdqr3WG-nueOLy5cVs4/edit#gid=652340147'
+            # link = 'https://docs.google.com/spreadsheets/d/1L0hm5yZPYfI5vxtTfRX2y4yEMl-ARyL2_nmplo88uw8/edit?gid=652340147#gid=652340147'
+            # # link = 'https://docs.google.com/spreadsheets/d/1W4jGEOW7YfbJZw9qTFHb65vxMbrlyb02XwJnpvx8BLs/edit#gid=652340147'
+            # link = 'https://docs.google.com/spreadsheets/d/1sjuv58WQwG3vfq7ikaRiUQxOVaF4tbu_XTgWhmamslU/edit?gid=652340147#gid=652340147' #krytyczynym
+            # link = 'https://docs.google.com/spreadsheets/d/1L0hm5yZPYfI5vxtTfRX2y4yEMl-ARyL2_nmplo88uw8/edit?gid=652340147#gid=652340147' #poczytajdziecku
+            # link = 'https://docs.google.com/spreadsheets/d/1rIN84s4N_5cQPcxIa343Smls8HH9tvObE4wT3B24iSw/edit#gid=652340147'
+            gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
+            table_df = gsheet_to_df(gsheetId, 'Posts')
+           
+            filtered_df = table_df[(table_df['do PBL'] == 'True') & (table_df["Link"].notna())]
+            accepted_records = len(filtered_df['Link'].tolist())
+            df.at[index, 'REKORDY ZAAKCEPTOWANE'] = accepted_records
+                    
+            # else:
+            #     df.at[index, 'REKORDY ZAAKCEPTOWANE'] = 'None'  
+       
         except (KeyError, TypeError):
-            df.at[index, 'REKORDY ZAAKCEPTOWANE'] = None  
-            df.at[index, 'REKORDY POZYSKANE'] = None
+            df.at[index, 'REKORDY ZAAKCEPTOWANE'] = 'None'
         except:
             df.at[index, 'REKORDY ZAAKCEPTOWANE'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
-            df.at[index, 'REKORDY POZYSKANE'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
 
     return df 
 
 
 
-def update_zakres_lat_w_zrodle(df):
-    """Updates the 'TEST' column in the DataFrame based on years extracted from 'Data publikacji'.
-    Args:
-        df (pd.DataFrame): The DataFrame containing the 'LINK' and 'TEST' columns.
-    Returns:
-        pd.DataFrame: The modified DataFrame with the updated 'TEST' column.
-    """
 
+def update_zakres_lat_w_zrodle(df):
     for index, row in tqdm(df.iterrows()):  
         try:
             link = row['LINK']
-            # link = 'https://docs.google.com/spreadsheets/d/1x3B02W8PuIsq83HknVwpFnFRUrQQTZvzVLFmNndKJgw/edit?gid=652340147#gid=652340147'
-            if not pd.isna(link):  # Check if 'LINK' is not null
+            if not pd.isna(link):  
+                # link = 'https://docs.google.com/spreadsheets/d/1T6JHJxlZJtAhUpYNS37CTbRmRN5Q4-_YYD6GXa2MADc'
                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
                 table_df = gsheet_to_df(gsheetId, 'Posts')
 
                 years_list = table_df['Data publikacji'].dropna().tolist()
-                if years_list:
+                if years_list != []:
                     first_year = min(years_list)
                     last_year = max(years_list)
                     df.at[index, 'ZAKRES LAT W ŹRÓDLE'] = f"{first_year}|{last_year}"  
                 else:
                     df.at[index, 'ZAKRES LAT W ŹRÓDLE'] = None  
         except (KeyError, TypeError):
-            df.at[index, 'ZAKRES LAT W ŹRÓDLE'] = None  
+            df.at[index, 'ZAKRES LAT W ŹRÓDLE'] = 'BRAK DATY PUBLIKACJI'  
         except:
-            df.at[index, 'ZAKRES LAT W ŹRÓDLE'] = 'Do uzupełnienia ręcznie'
+            df.at[index, 'ZAKRES LAT W ŹRÓDLE'] = 'Do sprawdzenia'
 
     return df
 
 
 
 def update_zakres_lat_oprac_rekordow(df):
-    """Updates the 'TEST' column in the DataFrame based on years extracted from 'Data publikacji'.
-    Args:
-        df (pd.DataFrame): The DataFrame containing the 'LINK' and 'TEST' columns.
-    Returns:
-        pd.DataFrame: The modified DataFrame with the updated 'TEST' column.
-    """
-
     for index, row in tqdm(df.iterrows()):  
         try:
             link = row['LINK']
-            if not pd.isna(link):  # Check if 'LINK' is not null
+            if not pd.isna(link): 
+                
                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
                 table_df = gsheet_to_df(gsheetId, 'Posts')
 
                 filtered_df = table_df[(table_df['do PBL'] == 'True') & (table_df["Data publikacji"].notna())]
                 years_list = filtered_df['Data publikacji'].tolist()
 
-                if years_list:
+                if years_list != []:
                         first_year = min(years_list)
                         last_year = max(years_list)
                         df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = f"{first_year}|{last_year}"
                 else:
-                        df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = None
+                        df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = "OPRACOWANIE NIEROZPOCZĘTE"
             else:
-                    df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = None
+                    df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = 'BRAK PRZYGOTOWANEJ TABELI'
                     
         except (KeyError, TypeError):
-            df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = None  
+            df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = 'BRAK DATY PUBLIKACJI'  
         except:
             df.at[index, 'ZAKRES LAT OPRAC. REKORDÓW'] = 'Do sprawdzenia'
 
@@ -129,42 +143,80 @@ dokumentacja_prace_manualne_df = gsheet_to_df('1jCjEaopxsezprUiauuYkwQcG1cp40Nqd
 dokumentacja_prace_manualne_df = dokumentacja_prace_manualne_df.loc[dokumentacja_prace_manualne_df['NAZWA'].notna()]   #Wyrzucenie pustych wierszy 
 dokumentacja_prace_manualne_df = dokumentacja_prace_manualne_df.rename(columns={'NAZWA':'LINK DO STRONY'}) #Zmiana nazwy kolumny, żeby była taka sama jak w kolejnej tabeli (web scraping)
 
+
 dokumentacja_web_scraping_df = gsheet_to_df('1-g_pgkvRIhBSKBENu_5HhRCMsHatv-eux3U_ERGHZG0', 'Raport')
 
 merged_df = pd.merge(dokumentacja_prace_manualne_df, dokumentacja_web_scraping_df, on='LINK DO STRONY', how='left')
 merged_df.columns   #Podgląd nazw kolumn, aby wybrać zbędne do wyrzucenia
 
 
-final_df = merged_df.drop(columns=['CZY DO MANUALNYCH PRAC? (WG ZAŁĄCZNIKA DO PROJEKTU)', 'DZIEDZINA', 'uwagi do manualnych prac', 'OSOBA OPRACOWUJĄCA', 'data utworzenia',  'web scraping do poprawki', 'Unnamed: 13', 'KTO ROBI?', 'NAZWA PLIKU', 'PLIK XLSX', 'PLIK JSON',  'AKTYWNY?', 'DODATKI', 'UWAGI', 'LINK DO KODU', 'CZY DO MANUALNYCH PRAC?', 'Unnamed: 14', 'CZY DO OPRAC. MANUALNEGO? [UWZGLĘDNIONE ZMIANY]', 'REKORDY'])
+final_df = merged_df.drop(columns=['CZY DO MANUALNYCH PRAC? (WG ZAŁĄCZNIKA DO PROJEKTU)', 'DZIEDZINA', 'uwagi do manualnych prac', 'data utworzenia',  'web scraping do poprawki', 'Unnamed: 13', 'KTO ROBI?', 'NAZWA PLIKU', 'PLIK XLSX', 'PLIK JSON',  'AKTYWNY?', 'DODATKI', 'UWAGI', 'LINK DO KODU', 'CZY DO MANUALNYCH PRAC?', 'Unnamed: 14', 'CZY DO OPRAC. MANUALNEGO? [UWZGLĘDNIONE ZMIANY]', 'REKORDY'])
+
+final_df_only_available_sources = final_df.loc[(final_df['CZY POZYSKANO?'] != 'REZYGNACJA') & (final_df['CZY POZYSKANO?'] != 'NIEDOSTĘPNA')]
 
 
 
-#Poniżej stworzenie DF tylko z serwisami do opracowania manualnego + wyrzucenie 1 serwisu, z ktorego zrezygnowano (2024-10-23 - 91 serwisów)
+final_df_only_automatic = final_df_only_available_sources.loc[final_df['CZY DO PRAC MANUALNYCH (PO ZMIANACH)'] == 'NIE']
+
 final_df_only_manual = final_df.loc[(final_df['CZY DO PRAC MANUALNYCH (PO ZMIANACH)'] == 'TAK')] 
 final_df_only_manual = final_df_only_manual.loc[(final_df['CZY POZYSKANO?'] != 'REZYGNACJA')] 
 final_df_only_manual = final_df_only_manual.reset_index(drop=True)
 
-#RAPORT DOT. ORACOWANIA MANUALNEGO
-print('RAPORT DOT. SERWISÓW DO OPRAC. MANUALNEGO')
-print("Liczba serwisów: " + str(final_df_only_manual.shape[0]))
-print('Zeskrobane: ' + str(final_df_only_manual['CZY POZYSKANO?'].value_counts()['TAK']))
-print('Zakończono opracowanie: ' + str(final_df_only_manual['STATUS PRAC'].value_counts()['zakończono']))
-print('Rozpoczęto opracowanie: ' + str(final_df_only_manual['STATUS PRAC'].value_counts()['rozpoczęto']))
-print('Przerwano opracowanie: ' + str(final_df_only_manual['STATUS PRAC'].value_counts()['przerwano']))
 
 
-# #Dodanie dwóch kolumn: 
-# final_df_only_manual[['ZAKRES LAT W ŹRÓDLE', 'ZAKRES LAT OPRAC. REKORDÓW']] = np.nan
-
+#%%DataFrame ze statystykami z dodatkowymi kolumnami (na potrzeby machine learningu)
 #Uruchomienie funkcji do wzbogacenia danych dot. ilosci zeskrobanych i zaakceptowanych rekordow z poszczegolncyh serwisow + zakresu dat
-update_rekordy_zaakceptowane(final_df_only_manual)  #Zlicza rekordy zaakceptowane w trakcie prac manualnych oraz wszystkie zeskrobane
+update_rekordy_pozyskane(final_df_only_manual)
+update_rekordy_zaakceptowane(final_df_only_manual)  #Nie liczy xksięgarni! link = 'https://docs.google.com/spreadsheets/d/1rIN84s4N_5cQPcxIa343Smls8HH9tvObE4wT3B24iSw/edit#gid=652340147
 update_zakres_lat_w_zrodle(final_df_only_manual)    
 update_zakres_lat_oprac_rekordow(final_df_only_manual)
 
 
+
+# final_df_only_manual['REKORDY POZYSKANE'] = final_df_only_manual['REKORDY POZYSKANE'].str.replace('\D', '', regex=True)
+# final_df_only_manual['REKORDY ZAAKCEPTOWANE'] = final_df_only_manual['REKORDY ZAAKCEPTOWANE'].str.replace('\D', '', regex=True)
+
+
+
+#%% RAPORT ZBIORCZY (OGÓLNY i OPRAC. MANUALNE)
+print('RAPORT z dnia ' + str(datetime.today().date()))
+print("Liczba wszystkich serwisów: " + str(final_df.shape[0]) + ' (w tym niedostępne)')
+print("Liczba wszystkich DOSTĘPNYCH serwisów: " + str(final_df_only_available_sources.shape[0]))
+print("Liczba serwisów do oprac. manualnego: " + str(final_df_only_manual.shape[0]))
+print("Liczba serwisów do oprac. automatycznego: " + str(final_df_only_automatic.shape[0]))
+print('Zeskrobane serwisy: ' + str(final_df['CZY POZYSKANO?'].value_counts()['TAK']))                     
+print('Zeskrobane serwisy (tylko do oprac. manualnego): ' + str(final_df_only_manual['CZY POZYSKANO?'].value_counts()['TAK']))
+
+
+
+print('PRACE MANUALNE')
+print('Zakończono opracowanie: ' + str(final_df['STATUS PRAC'].value_counts()['zakończono']))        
+print('Rozpoczęto opracowanie: ' + str(final_df['STATUS PRAC'].value_counts()['rozpoczęto']))
+print('Przerwano opracowanie: ' + str(final_df['STATUS PRAC'].value_counts()['przerwano']))
+print('Gotowe do przydzielenia: ' + str(final_df_only_manual.loc[(final_df_only_manual['czy_przekazano_do_manual'] == 'tak') & (final_df_only_manual['OSOBA OPRACOWUJĄCA'].isna())].shape[0]))
+
+print('Opracowane rekordy: ' + str(final_df_only_manual['REKORDY ZAAKCEPTOWANE'].replace(to_replace='None', value=np.nan).dropna().sum())) #Trochę więcej. Sprawdzić ze statystykami
+
+### KOD PONIŻEJ GENERUJE LICZBY KTÓRE NIE ZGADZAJĄ SIE Z TABELAMI. ZBADAC SPRAWĘ
+# print('Zeskrobane rekordy (do opracowania manualnego): ' + str(final_df_only_manual['REKORDY POZYSKANE'].replace(to_replace='None', value=np.nan).dropna().astype('int64').sum()))  #151459 powinno być○ ponad 180 000 (2024-10-24) BŁ◘AD bo niektóre są zeskrobane do automatycznego opracowania
+# print('Zeskrobane rekordy (wszystkie): ' + str(final_df['REKORDY POZYSKANE'].replace(to_replace='None', value=np.nan).dropna().str.replace('\D', '', regex=True).astype('int64').sum())) 
+      
+
+#POPRAWIĆ FUNKCJĘ update_rekordy_zaakceptowane
+
+
+#ZAPISZ RAPORT w jakim formacie i wyslij do arkusza
+
+
+#Checking raport
+
+if final_df_only_manual.shape[0] + final_df_only_automatic.shape[0] == final_df_only_available_sources.shape[0]:
+    print("Wszystko się zgadza")
+
+
+
 #Wyrzucenie zbędnych dla uczenia maszynowego kolumn:
-machine_learning_df = final_df_only_manual.drop(columns=['CZY DO PRAC MANUALNYCH (PO ZMIANACH)', 'czy_przekazano_do_manual', 'CZY POZYSKANO?'])
-machine_learning_df = machine_learning_df.drop(columns=['STATUS PRAC'])
+machine_learning_df = final_df_only_manual.drop(columns=['CZY DO PRAC MANUALNYCH (PO ZMIANACH)', 'czy_przekazano_do_manual', 'CZY POZYSKANO?', 'OSOBA OPRACOWUJĄCA', 'STATUS PRAC'])
 machine_learning_df = machine_learning_df.rename(columns={'LINK DO ARKUSZA': 'NAZWA PLIKU', 'LINK':'LINK DO PLIKU'})
 
 #Utworzyć z tego plik xlsx
@@ -174,6 +226,12 @@ machine_learning_df = machine_learning_df.rename(columns={'LINK DO ARKUSZA': 'NA
 with pd.ExcelWriter(f"data\iPBL_manual_works_stats_{datetime.today().date()}.xlsx", engine='xlsxwriter') as writer:    
     machine_learning_df.to_excel(writer, 'Posts', index=False, encoding='utf-8')   
     writer.save()   
+
+
+
+
+
+
 
 
 
@@ -187,7 +245,6 @@ with pd.ExcelWriter(f"data\iPBL_manual_works_stats_{datetime.today().date()}.xls
 # 	gfile = drive.CreateFile({'parents': [{'id': '1jCjEaopxsezprUiauuYkwQcG1cp40NqdhvxIzG5qUu8'}]})  
 # 	gfile.SetContentFile(upload_file)
 # # 	gfile.Upload()        
-
 
 
 # gauth = GoogleAuth()           
@@ -204,94 +261,31 @@ with pd.ExcelWriter(f"data\iPBL_manual_works_stats_{datetime.today().date()}.xls
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
- #DO USUNIECIA
-# for index, row in tqdm(final_df_only_manual.iterrows()):
-#     link = row['LINK'] 
-#     try:
-#         # link = 'https://docs.google.com/spreadsheets/d/1H44U6tl1OWl5Y2ScHyp__OlUR7vab-zbyBkqAQKZryE'
-#         gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
-#         table_df = gsheet_to_df(gsheetId, 'Posts')
-#         first_year = re.search(r'\d\d\d\d', table_df['Data publikacji'][0]).group()
-#         last_row_number = table_df['Data publikacji'].dropna().shape[0]
-#         last_year = re.search(r'\d\d\d\d', table_df.iloc[last_row_number-1,1]).group()
-        
-#         # final_df_only_manual.loc[index, row['TEST']] = last_year
-        
-#         # if last_year < first_year:
-#         #     row['TEST'] = str(last_year) + "-" + str(first_year)
-#         # else:
-#         #     row['TEST'] = str(first_year) + "-" + str(last_year)
-            
-#     except (KeyError, TypeError):
-#         row['TEST'] = None
-
-
-# def update_liczba_zapisow(df):
-
-#     for index, row in df.iterrows():  
+# def update_rekordy_zaakceptowane(df):
+#     for index, row in tqdm(df.iterrows()):  
 #         try:
 #             link = row['LINK']
 #             if not pd.isna(link):  # Check if 'LINK' is not null
+#                 link = 'https://docs.google.com/spreadsheets/d/1zOxDVFLvk2ovJEyyKp-Zpw6Vqdqr3WG-nueOLy5cVs4/edit#gid=652340147'
 #                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
 #                 table_df = gsheet_to_df(gsheetId, 'Posts')
-
 #                 all_extracted_records = table_df['Link'].dropna().shape[0]
+#                 df.at[index, 'REKORDY POZYSKANE'] = all_extracted_records
+               
 #                 filtered_df = table_df[(table_df['do PBL'] == 'True') & (table_df["Data publikacji"].notna())]
 #                 accepted_records = len(filtered_df['Link'].tolist())
-                
-#                 df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = all_extracted_records
-#                 df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = accepted_records
-                
+#                 df.at[index, 'REKORDY ZAAKCEPTOWANE'] = accepted_records
+                        
 #             else:
-#                 df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = None  
-#                 df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = None
+#                 df.at[index, 'REKORDY ZAAKCEPTOWANE'] = 'BRAK PRZYGOTOWANEJ TABELI'  
+#                 df.at[index, 'REKORDY POZYSKANE'] = 'BRAK PRZYGOTOWANEJ TABELI'
+       
 #         except (KeyError, TypeError):
-#             df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = None  
-#             df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = None
+#             df.at[index, 'REKORDY ZAAKCEPTOWANE'] = None  
+#             df.at[index, 'REKORDY POZYSKANE'] = None
 #         except:
-#             df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
-#             df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
-
-#     return df 
-
-
-# def update_liczba_zapisow(df):
-
-#     for index, row in df.iterrows():  
-#         try:
-#             link = row['LINK']
-#             if not pd.isna(link):  # Check if 'LINK' is not null
-#                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
-#                 table_df = gsheet_to_df(gsheetId, 'Posts')
-
-#                 all_extracted_records = table_df['Link'].dropna().shape[0]
-#                 filtered_df = table_df[(table_df['do PBL'] == 'True') & (table_df["Data publikacji"].notna())]
-#                 accepted_records = len(filtered_df['Link'].tolist())
-                
-#                 df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = all_extracted_records
-#                 df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = accepted_records
-                
-#             else:
-#                 df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = None  
-#                 df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = None
-#         except (KeyError, TypeError):
-#             df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = None  
-#             df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = None
-#         except:
-#             df.at[index, 'LICZBA ZESKROBANYCH REKORDÓW'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
-#             df.at[index, 'LICZBA OPRACOWANYCH REKORDÓW'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
+#             df.at[index, 'REKORDY ZAAKCEPTOWANE'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
+#             df.at[index, 'REKORDY POZYSKANE'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
 
 #     return df 
 
