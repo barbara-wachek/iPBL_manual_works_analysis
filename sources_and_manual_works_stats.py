3a58eea88b2e96c3ed9a5092a925d36617e99627
Barbara Wachek <barbara.wachek@gmail.com>
2024-10-29 09:26:24 +0100

    2024-10-29

--- sources_and_manual_works_stats.py
+++ sources_and_manual_works_stats.py
@@ -30,31 +30,31 @@ def gsheet_to_df(gsheetId, worksheet):
     df = get_as_dataframe(sheet.worksheet(worksheet), evaluate_formulas=True, dtype=str).dropna(how='all').dropna(how='all', axis=1)
     return df
 
+#Raczej nie używać poniższej funkcji. Do późniejszej poprawki 
+# def update_rekordy_pozyskane(df, max_retries=3, backoff_factor=2):
+#     for index, row in tqdm(df.iterrows()):
+#         link = row['LINK']
+#         retries = 0
+#         while retries < max_retries:
+#             try:
+#                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
+#                 table_df = gsheet_to_df(gsheetId, 'Posts')
+#                 all_extracted_records = table_df['Link'].dropna().shape[0]
+#                 df.at[index, 'REKORDY POZYSKANE'] = all_extracted_records
+#                 break  # Wyjdź z pętli, jeśli pobranie danych się powiodło
+#             except requests.exceptions.RequestException as e:
+#                 print(f"Błąd podczas pobierania danych dla wiersza {index}: {e}")
+#                 retries += 1
+#                 # Wykładniczy backoff: zwiększaj czas oczekiwania po każdej próbie
+#                 wait_time = backoff_factor ** retries
+#                 time.sleep(wait_time)
+#             except Exception as e:
+#                 df.at[index, 'REKORDY POZYSKANE'] = "Błąd"
+#                 break
+#         else:  # Jeśli pętla się wyczerpała
+#             df.at[index, 'REKORDY POZYSKANE'] = "Błąd po wielu próbach"
 
-def update_rekordy_pozyskane(df, max_retries=3, backoff_factor=2):
-    for index, row in tqdm(df.iterrows()):
-        link = row['LINK']
-        retries = 0
-        while retries < max_retries:
-            try:
-                gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
-                table_df = gsheet_to_df(gsheetId, 'Posts')
-                all_extracted_records = table_df['Link'].dropna().shape[0]
-                df.at[index, 'REKORDY POZYSKANE'] = all_extracted_records
-                break  # Wyjdź z pętli, jeśli pobranie danych się powiodło
-            except requests.exceptions.RequestException as e:
-                print(f"Błąd podczas pobierania danych dla wiersza {index}: {e}")
-                retries += 1
-                # Wykładniczy backoff: zwiększaj czas oczekiwania po każdej próbie
-                wait_time = backoff_factor ** retries
-                time.sleep(wait_time)
-            except Exception as e:
-                df.at[index, 'REKORDY POZYSKANE'] = 0
-                break
-        else:  # Jeśli pętla się wyczerpała
-            df.at[index, 'REKORDY POZYSKANE'] = "Błąd po wielu próbach"
-
-    return df
+#     return df
 
 
 def update_rekordy_zaakceptowane(df, max_retries=3, backoff_factor=2):
@@ -110,52 +110,6 @@ def update_zakres_dat_w_zrodle(df):
     return df
 
 
-#GEMINI
-
-# def update_zakres_dat_w_zrodle(df, max_retries=3, backoff_factor=2):
-#     """
-#     Aktualizuje zakres dat w źródle dla każdego wiersza DataFrame, obsługując błędy połączenia.
-
-#     Args:
-#         df (pandas.DataFrame): DataFrame do aktualizacji.
-#         max_retries (int, optional): Maksymalna liczba prób pobrania danych.
-#         backoff_factor (int, optional): Współczynnik wykładniczego zwiększania czasu oczekiwania między próbami.
-
-#     Returns:
-#         pandas.DataFrame: Zaktualizowany DataFrame.
-#     """
-
-#     for index, row in tqdm(df.iterrows()):
-#         link = row['LINK']
-#         retries = 0
-#         while retries < max_retries:
-#             try:
-#                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
-#                 table_df = gsheet_to_df(gsheetId, 'Posts')
-
-#                 dates_list = table_df['Data publikacji'].dropna().tolist()
-#                 if dates_list:
-#                     first_date = min(dates_list)
-#                     last_date = max(dates_list)
-#                     df.at[index, 'ZAKRES DAT W ŹRÓDLE'] = f"{first_date}|{last_date}"
-#                 else:
-#                     df.at[index, 'ZAKRES DAT W ŹRÓDLE'] = 'Brak daty publikacji'
-#                 break
-#             except requests.exceptions.RequestException as e:
-#                 print(f"Błąd połączenia dla wiersza {index}: {e}")
-#                 retries += 1
-#                 wait_time = backoff_factor ** retries
-#                 time.sleep(wait_time)
-#             except Exception as e:
-#                 df.at[index, 'ZAKRES DAT W ŹRÓDLE'] = 'Błąd przetwarzania danych'
-#                 break
-#         else:
-#             df.at[index, 'ZAKRES DAT W ŹRÓDLE'] = "Błąd po wielu próbach"
-
-#     return df
-
-
-
 
 
 def update_zakres_dat_oprac_rekordow(df):
@@ -192,7 +146,8 @@ def update_zakres_dat_oprac_rekordow(df):
 dokumentacja_prace_manualne_df = gsheet_to_df('1jCjEaopxsezprUiauuYkwQcG1cp40NqdhvxIzG5qUu8', 'dokumentacja')
 dokumentacja_prace_manualne_df = dokumentacja_prace_manualne_df.loc[dokumentacja_prace_manualne_df['NAZWA'].notna()]   #Wyrzucenie pustych wierszy 
 dokumentacja_prace_manualne_df = dokumentacja_prace_manualne_df.rename(columns={'NAZWA':'LINK DO STRONY'}) #Zmiana nazwy kolumny, żeby była taka sama jak w kolejnej tabeli (web scraping)
-
+rekordy_pozyskane_wg_tabeli_oprac_manualne = dokumentacja_prace_manualne_df['REKORDY POZYSKANE'].fillna('0').astype("int64").sum()
+# dokumentacja_prace_manualne_df['REKORDY ZAAKCEPTOWANE'].fillna('0').astype("int64").sum() #Te wartosci są praktycznie puste w tabeli
 
 dokumentacja_web_scraping_df = gsheet_to_df('1-g_pgkvRIhBSKBENu_5HhRCMsHatv-eux3U_ERGHZG0', 'Raport')
 
@@ -204,25 +159,40 @@ final_df = merged_df.drop(columns=['CZY DO MANUALNYCH PRAC? (WG ZAŁĄCZNIKA DO 
 
 final_df_only_available_sources = final_df.loc[(final_df['CZY POZYSKANO?'] != 'REZYGNACJA') & (final_df['CZY POZYSKANO?'] != 'NIEDOSTĘPNA')]
 
-
-
 final_df_only_automatic = final_df_only_available_sources.loc[final_df['CZY DO PRAC MANUALNYCH (PO ZMIANACH)'] == 'NIE']
+
+
 
 final_df_only_manual = final_df.loc[(final_df['CZY DO PRAC MANUALNYCH (PO ZMIANACH)'] == 'TAK')] 
 final_df_only_manual = final_df_only_manual.loc[(final_df['CZY POZYSKANO?'] != 'REZYGNACJA')] 
 final_df_only_manual = final_df_only_manual.reset_index(drop=True)
 
-
+rekordy_pozyskane_tylko_manualne_na_podstawie_tabeli = final_df_only_manual['REKORDY POZYSKANE'].fillna('0').astype("int64").sum()
 
 #%%DataFrame ze statystykami z dodatkowymi kolumnami (na potrzeby machine learningu)
 #Uruchomienie funkcji do wzbogacenia danych dot. ilosci zeskrobanych i zaakceptowanych rekordow z poszczegolncyh serwisow + zakresu dat
 
-update_rekordy_pozyskane(final_df_only_manual)
+
+#Funkcja update_rekordy_pozyskane zawsze zwraca inne wartosci niz sa w tabeli. W tabeli sa wartosci wpisywane z reki, wiec tez moga troche sie roznic w stosunku do rzeczywistosci
+# update_rekordy_pozyskane(final_df_only_manual)
+# #zamekczyta zwraca błąd
+# #Checking
+# if rekordy_pozyskane_tylko_manualne_na_podstawie_tabeli == final_df_only_manual['REKORDY POZYSKANE'].sum():
+#     print("Zgadza się z tabelą z Dysku")
+# else: 
+#     x = final_df_only_manual['REKORDY POZYSKANE'].sum() - rekordy_pozyskane_tylko_manualne_na_podstawie_tabeli
+#     print(f"Różnica między plikami to: {x}")
+#  #Różnica miedzy plikami: -32359
+    
+        
+    
 update_rekordy_zaakceptowane(final_df_only_manual)  
 
+extracted_not_assigned = final_df_only_manual.loc[(final_df_only_manual['REKORDY ZAAKCEPTOWANE'] == 0) & (final_df_only_manual['KTO'].isna()) & (final_df_only_manual['LINK DO ARKUSZA'].notna())] #Serwisy zeskrobane, ale jeszcze nieprzydzielone. Gotowe do przydzielenia
+extracted_and_assigned_not_processed = final_df_only_manual.loc[(final_df_only_manual['REKORDY ZAAKCEPTOWANE'] == 0) & (final_df_only_manual['KTO'].notna()) & (final_df_only_manual['LINK DO ARKUSZA'].notna())] #0 zaakceptowanych, ale przydzielone. Do sprawdzenia czy jeszcze nie rozpoczęły te osoby prac, czy moze czegos nie policzylo
 
-final_df_only_manual['REKORDY POZYSKANE'].sum() #Zlivzy tylko te pozyskane do oprac manualnego 157137
-final_df_only_manual['REKORDY ZAAKCEPTOWANE'].sum() #16721  #17662  Nie wiem dlaczego ale rozne liczb7 za kazdym razem podaje
+
+final_df_only_manual['REKORDY ZAAKCEPTOWANE'].sum() #16721  #17662  Nie wiem dlaczego ale rozne liczb7 za kazdym razem podaje 2024-10-29: 17692
 
 
 #Funkcje ponizej raczej dzialaja poprawnie
@@ -256,19 +226,40 @@ print('Przerwano opracowanie: ' + str(final_df['STATUS PRAC'].value_counts()['pr
 
 
 #W poniższych sa błedy. do zbadania
-print('Gotowe do przydzielenia: ' + str(final_df_only_manual.loc[(final_df_only_manual['czy_przekazano_do_manual'] == 'tak') & (final_df_only_manual['KTO'].isna())].shape[0]))
+print('Serwisy gotowe do przydzielenia (są tabelki): ' + str(final_df_only_manual.loc[(final_df_only_manual['czy_przekazano_do_manual'] == 'tak') & (final_df_only_manual['KTO'].isna())].shape[0]))
 
-print('Opracowane rekordy: ' + str(final_df_only_manual['REKORDY ZAAKCEPTOWANE'].replace(to_replace='None', value=np.nan).dropna().sum())) #Trochę więcej. Sprawdzić ze statystykami
-
-### KOD PONIŻEJ GENERUJE LICZBY KTÓRE NIE ZGADZAJĄ SIE Z TABELAMI. ZBADAC SPRAWĘ
-# print('Zeskrobane rekordy (do opracowania manualnego): ' + str(final_df_only_manual['REKORDY POZYSKANE'].replace(to_replace='None', value=np.nan).dropna().astype('int64').sum()))  #151459 powinno być○ ponad 180 000 (2024-10-24) BŁ◘AD bo niektóre są zeskrobane do automatycznego opracowania
-# print('Zeskrobane rekordy (wszystkie): ' + str(final_df['REKORDY POZYSKANE'].replace(to_replace='None', value=np.nan).dropna().str.replace('\D', '', regex=True).astype('int64').sum())) 
-      
-
-#POPRAWIĆ FUNKCJĘ update_rekordy_zaakceptowane
+print('Rekordy zaakceptowane do PBL: ' + str(final_df_only_manual['REKORDY ZAAKCEPTOWANE'].sum())) #Trochę więcej. Sprawdzić ze statystykami
 
 
-#ZAPISZ RAPORT w jakim formacie i wyslij do arkusza
+# Przygotowanie danych do DataFrame 
+report_data = {
+    'Opis': ['Data raportu', 'Liczba wszystkich serwisów', 'Liczba wszystkich DOSTĘPNYCH serwisów', 'Liczba serwisów do oprac. manualnego', 'Liczba serwisów do oprac. automatycznego', 'Zeskrobane serwisy','Zeskrobane serwisy (tylko do oprac. manualnego)', 'Zakończono opracowanie','Rozpoczęto opracowanie', 'Przerwano opracowanie', 'Serwisy gotowe do przydzielenia (są tabelki)', 'Rekordy zaakceptowane do PBL'],
+    'Wartość': [datetime.today().date(), final_df.shape[0], final_df_only_available_sources.shape[0], final_df_only_manual.shape[0], final_df_only_automatic.shape[0], final_df['CZY POZYSKANO?'].value_counts()['TAK'], final_df_only_manual['CZY POZYSKANO?'].value_counts()['TAK'], final_df['STATUS PRAC'].value_counts()['zakończono'], final_df['STATUS PRAC'].value_counts()['rozpoczęto'], final_df['STATUS PRAC'].value_counts()['przerwano'], final_df_only_manual.loc[(final_df_only_manual['czy_przekazano_do_manual'] == 'tak') & (final_df_only_manual['KTO'].isna())].shape[0], final_df_only_manual['REKORDY ZAAKCEPTOWANE'].sum()]
+}
+
+# Tworzenie DataFrame
+df = pd.DataFrame(report_data)
+
+# Zapis do Excela
+with pd.ExcelWriter(f"data\\raport.xlsx") as writer:
+    df.to_excel(writer, sheet_name='Raport', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+### KOMENTARZE
+#Funkcja update_rekordy_pozyskane - raczej nie używac i pobierac zawartosci z tabeli, która bazuje na uzupełnieniach ręcznych. Przy korzystaniu z funkcji co rusz inne liczby zwraca + nie ma jak zliczyc serwisów, które są opracowane, ale jeszcze nie ma plików w tabeli do oprac. manualnego (np. Culture.pl - tylko json)
+#ZAPISZ RAPORT w jakim formacie i wyslij do arkusza? Zastanowić się
+#Napisać testy jednostkowe dla tego pliku 
+
 
 
 
@@ -294,59 +285,12 @@ with pd.ExcelWriter(f"data\iPBL_manual_works_stats_{datetime.today().date()}.xls
 
 
 
-#%% Przesłanie tabeli DOPRACUJ
-
-# gauth = GoogleAuth()           
-# drive = GoogleDrive(gauth)   
-      
-# upload_file_list = [f"krytycznym_okiem_{datetime.today().date()}.xlsx", f'krytycznym_okiem_{datetime.today().date()}.json']
-# for upload_file in upload_file_list:
-# 	gfile = drive.CreateFile({'parents': [{'id': '1jCjEaopxsezprUiauuYkwQcG1cp40NqdhvxIzG5qUu8'}]})  
-# 	gfile.SetContentFile(upload_file)
-# # 	gfile.Upload()        
-
-
-# gauth = GoogleAuth()           
-# drive = GoogleDrive(gauth)   
-      
-# upload_file= f"iPBL_manual_works_stats_{datetime.today().date()}.xlsx"
-
-# gfile = drive.CreateFile({'parents': [{'id': '1ZrLyjsA6Q-k78M8gpuK5EB2NXCk56zA0'}]})  
-# gfile.SetContentFile(upload_file)
-# gfile.Upload()         
 
 
 
 
 
 
-# def update_rekordy_zaakceptowane(df):
-#     for index, row in tqdm(df.iterrows()):  
-#         try:
-#             link = row['LINK']
-#             if not pd.isna(link):  # Check if 'LINK' is not null
-#                 link = 'https://docs.google.com/spreadsheets/d/1zOxDVFLvk2ovJEyyKp-Zpw6Vqdqr3WG-nueOLy5cVs4/edit#gid=652340147'
-#                 gsheetId = re.search('(?<=https:\/\/docs\.google\.com\/spreadsheets\/d\/)[A-Za-z\d\_\-]*', link).group(0)
-#                 table_df = gsheet_to_df(gsheetId, 'Posts')
-#                 all_extracted_records = table_df['Link'].dropna().shape[0]
-#                 df.at[index, 'REKORDY POZYSKANE'] = all_extracted_records
-               
-#                 filtered_df = table_df[(table_df['do PBL'] == 'True') & (table_df["Data publikacji"].notna())]
-#                 accepted_records = len(filtered_df['Link'].tolist())
-#                 df.at[index, 'REKORDY ZAAKCEPTOWANE'] = accepted_records
-                        
-#             else:
-#                 df.at[index, 'REKORDY ZAAKCEPTOWANE'] = 'BRAK PRZYGOTOWANEJ TABELI'  
-#                 df.at[index, 'REKORDY POZYSKANE'] = 'BRAK PRZYGOTOWANEJ TABELI'
-       
-#         except (KeyError, TypeError):
-#             df.at[index, 'REKORDY ZAAKCEPTOWANE'] = None  
-#             df.at[index, 'REKORDY POZYSKANE'] = None
-#         except:
-#             df.at[index, 'REKORDY ZAAKCEPTOWANE'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
-#             df.at[index, 'REKORDY POZYSKANE'] = "DO UZUPEŁNIENIA RĘCZNIE?" 
-
-#     return df 
 
 
 
